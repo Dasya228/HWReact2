78e2d239fcd8892478f9943e9a5290905c51e2b1
Пользователь <world.worensi@gmail.com>
2023-11-20 16:38:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [state, setState] = useState(0)
+    const [inputValue, setInput] = useState('')
+
+ const handleCalculator = (num)=>{
+        if(num==='AC'){
+            setState(0)
+            setInput('')
+        }else{
+            setState(eval(inputValue));
+            setInput(`${eval(inputValue)}${num}`);
+        }
+
+ }
+
+
+
+
+const handleNumber=(num)=>{
+    setInput(`${inputValue}${num}`);
+
 }
 
-export default App;
+    return (
+        <div className={'container'}>
+            <input type="text" value={inputValue}/>
+
+
+
+
+
+            <button className={'number'} onClick={()=>handleNumber(7)}>7</button>
+            <button className={'number'} onClick={()=>handleNumber(8)}>8</button>
+            <button className={'number'} onClick={()=>handleNumber(9)}>9</button>
+            <button className={'symbol'} onClick={()=>handleCalculator('/')}>/</button>
+
+            <button className={'number'} onClick={()=>handleNumber(6)}>6</button>
+            <button className={'number'} onClick={()=>handleNumber(5)}>5</button>
+            <button className={'number'} onClick={()=>handleNumber(4)}>4</button>
+            <button className={'symbol'} onClick={()=>handleCalculator('*')}>*</button>
+
+            <button className={'number'} onClick={()=>handleNumber(3)}>3</button>
+            <button className={'number'} onClick={()=>handleNumber(2)}>2</button>
+            <button className={'number'} onClick={()=>handleNumber(1)}>1</button>
+            <button className={'symbol'} onClick={()=>handleCalculator('-')}>-</button>
+
+
+            <button className={'symbol'} onClick={()=>handleCalculator('+')}>+</button>
+            <button className={'number'} onClick={()=>handleNumber(0)}>0</button>
+             <button className={'symbol'} onClick={()=>handleCalculator(' ')}>=</button>
+            <button className={'symbol'} onClick={()=>handleCalculator('AC')}>AC</button>
+
+
+
+
+            {/*<Btn text={"Hello"}/>*/}
+            {/*<Btn text={"Dastan"}/>*/}
+            {/*<Btn text={"Click"}/>*/}
+            {/*<Btn text={"Aidin"}/>*/}
+
+        </div>
+    )
+}
+
+export default App
